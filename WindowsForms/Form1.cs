dbf72398d44cab273784cf655e140ebc722e696f
sandydev6 <santhanaraman.sivasankaran@outlook.com>
2023-03-29 15:37:19 +0530

    test

--- WindowsForms/Form1.cs
+++ WindowsForms/Form1.cs
@@ -16,5 +16,10 @@ namespace WindowsForms
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //test
+        }
     }
 }
